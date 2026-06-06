bf12044f78024a3ec59fb8d52071adb3b61ba2d2
Asin Omal <127041794+AsinOmal@users.noreply.github.com>
2026-06-07 04:18:53 +0530

    chore: update deployment config

--- client/public/config.js
+++ client/public/config.js
@@ -3,5 +3,5 @@ window.env = {
   VITE_ASGARDEO_ORG_NAME: "",
   VITE_SIGN_IN_REDIRECT_URL: "",
   VITE_SIGN_OUT_REDIRECT_URL: "",
-  VITE_API_BASE_URL: ""
+  VITE_API_BASE_URL: "https://3cb61e27-89e3-425a-8362-2b938772eb42-dev.e1-us-east-azure.choreoapis.dev/restaurant-ordering/server/v1.0"
 };
